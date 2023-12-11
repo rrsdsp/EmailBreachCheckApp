02c641490b693baf4a9aa0dd295c9ab26b0aeb18
Roman Rozman <rozman.roman@windowslive.com>
2023-12-11 02:50:10 +0100

    Popravek

--- EmailBreachCheckApi/Program.cs
+++ EmailBreachCheckApi/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.Services.AddTransient<ICacheGrain, CacheGrain>();
+
 
 var app = builder.Build();
 
